22032d73f7ab860fbe4a1139dbc5b276c1f49f90
Sebastian Geiger <sbastig@gmx.net>
2019-05-14 21:55:26 +0200

    model-checking: fallback to hard-coded json
    When no argument has been given, fallback to a hard-coded json.

--- main.c
+++ main.c
@@ -26,24 +26,33 @@ int main (int argc, char *argv[])
     printf ("input file name: %s\n", input_filename);
   }
 
-  GFile *input_file = g_file_new_for_path (input_filename);
-  GError *load_error = NULL;
-
   char *content = NULL;
 
-  gboolean loaded =
-      g_file_load_contents (input_file,
-                            NULL,
-                            &content,
-                            NULL,
-                            NULL,
-                            &load_error);
+  gboolean loaded = FALSE;
 
+  if (input_filename != NULL)
+  {
+    GError *load_error = NULL;
+
+    GFile *input_file = g_file_new_for_path (input_filename);
+
+    loaded = g_file_load_contents (input_file, NULL, &content, NULL, NULL,
+                                   &load_error);
+  }
+
+  // fallback to hard-coded kripke json if no arguments were passed.
   if (!loaded)
   {
-    printf ("Could not read input file: %s\n", input_filename);
-
-    return EXIT_FAILURE;
+    content = "{\n"
+              "  \"states\": [\n"
+              "    { \"name\": \"s0\", \"labels\": [ \"a\", \"bar\" ] },\n"
+              "    { \"name\": \"s1\", \"labels\": [ \"a\" ] },\n"
+              "    { \"name\": \"s2\", \"labels\": [ \"bar\" ] },\n"
+              "    { \"name\": \"s3\", \"labels\": [ \"a\", \"bar\" ] }\n"
+              "  ],\n"
+              "  \"initialStates\": [ \"s0\" ],\n"
+              "  \"relations\": [ [ \"s0\", \"s1\" ], [ \"s1\", \"s2\" ], [ \"s1\", \"s0\" ], [ \"s2\", \"s3\" ], [ \"s2\", \"s0\" ], [ \"s3\", \"s3\" ] ]\n"
+              "}";
   }
 
   kripke = kripke_structure_new_from_string (content, &error);
